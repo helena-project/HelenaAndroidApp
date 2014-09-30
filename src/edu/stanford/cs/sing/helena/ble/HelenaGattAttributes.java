b89883e4b09236bbb71dcf67ef8e1edf949ef73c
Northshoot <laurynas.riliskis@gmail.com>
2014-09-30 16:26:40 -0700

    added bluethooth sceleton stuff

---
+++ src/edu/stanford/cs/sing/helena/ble/HelenaGattAttributes.java
@@ -0,0 +1,23 @@
+package edu.stanford.cs.sing.helena.ble;
+
+import java.util.HashMap;
+
+public class HelenaGattAttributes {
+
+	private static HashMap<String, String> attributes = new HashMap<String, String>();
+	public static final String UUID_LISTED_DEVICE = "00009430-0000-1000-8000-00805f9b34fb";
+	public static final String HELENA_SERVICE = "00009431-0000-1000-8000-00805f9b34fb";
+    
+
+
+    static {
+        attributes.put(HELENA_SERVICE, "Helena Service");
+        attributes.put(UUID_LISTED_DEVICE, "Seen Device");
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = attributes.get(uuid);
+        return name == null ? defaultName : name;
+    }
+
+}
